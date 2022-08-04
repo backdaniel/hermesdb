9b091220a04bd3cd1a72f9f16ffcfc984985be83
backdaniel <mail@backdaniel.com>
2022-08-04 17:33:50 -0300

    Update and rename new-entry.py to generate-entry.py

--- generate-entry.py
+++ generate-entry.py
@@ -5,13 +5,13 @@ definition = 'a thing characteristic of its kind or illustrating a general rule'
 difficulty = 16
 
 term = term.ljust(16, ' ')[:16]
-definition = definition.ljust(128, ' ')[:128]
+definition = definition.ljust(108, ' ')[:108]
 
 while True:
 	nonce = ''.join(random.choices(string.printable[:95], k=32))
 	entry = term + ' ' + definition + ' ' + nonce
-	result = hashlib.sha3_256(entry.encode()).hexdigest()
-	binary = str(bin(int(result, 16)))[2:].rjust(256, '0')
+	result = hashlib.sha3_512(entry.encode()).hexdigest()
+	binary = str(bin(int(result, 16)))[2:].rjust(512, '0')
 	print(binary[:difficulty])
 	if not '1' in binary[:difficulty]:
 		print(entry)
