41d0ae193c43d5f422782228b567009f01c58151
backdaniel <mail@backdaniel.com>
2022-08-04 16:54:38 -0300

    Add files via upload

---
+++ validate.py
@@ -0,0 +1,18 @@
+import random, string, hashlib
+
+term = 'example'
+definition = 'a thing characteristic of its kind or illustrating a general rule'
+difficulty = 16
+
+term = term.ljust(16, ' ')[:16]
+definition = definition.ljust(128, ' ')[:128]
+
+while True:
+	nonce = ''.join(random.choices(string.printable[:95], k=32))
+	parts = term + ' ' + definition + ' ' + nonce
+	result = hashlib.sha3_256(parts.encode()).hexdigest()
+	binary = str(bin(int(result, 16)))[2:].rjust(256, '0')
+	print(binary[:difficulty])
+	if not '1' in binary[:difficulty]:
+		print(parts)
+		break
