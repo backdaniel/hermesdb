3068857ca147bae02bb00ababab5a98789b11ba7
backdaniel <mail@backdaniel.com>
2022-08-04 19:33:14 -0300

    Update get-difficulty.py

--- scripts/get-difficulty.py
+++ scripts/get-difficulty.py
@@ -1,6 +1,6 @@
 import hashlib
 
-entry = 'example          a thing characteristic of its kind or illustrating a general rule                                            c4CFw7 ]"g#$a)k.G\fB`:6KNi:aO\'O!'
+entry = 'example          a thing characteristic of its kind or illustrating a general rule                                            c4CFw7 ]"g#$a)k.G\\fB`:6KNi:aO\'O!'
 
 result = hashlib.sha3_512(entry.encode()).hexdigest()
 binary = str(bin(int(result, 16)))[2:].rjust(512, '0')
