64fe925c5def20b29b01079bc6705bf1abad59eb
backdaniel <mail@backdaniel.com>
2022-08-04 17:05:20 -0300

    Update get-difficulty.py

--- get-difficulty.py
+++ get-difficulty.py
@@ -1,6 +1,7 @@
 import hashlib
 
 entry = 'example          a thing characteristic of its kind or illustrating a general rule                                                                KUSI3vXOGI(2sd R.Q!=IVPB0|cb (.L'
+
 result = hashlib.sha3_256(entry.encode()).hexdigest()
 binary = str(bin(int(result, 16)))[2:].rjust(256, '0')
 difficulty = len(binary.split('1', 1)[0])
