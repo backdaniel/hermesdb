c2996312d2bbebeb42a7df44d8298f55324f878e
backdaniel <mail@backdaniel.com>
2022-12-17 20:55:02 -0300

    Update get-difficulty.py

--- get-difficulty.py
+++ get-difficulty.py
@@ -1,8 +1,7 @@
-import hashlib
+import hashlib, sys
 
-entry = 'example          a thing characteristic of its kind or illustrating a general rule                                            c4CFw7 ]"g#$a)k.G\\fB`:6KNi:aO\'O!'
-
-result = hashlib.sha3_512(entry.encode()).hexdigest()
+result = hashlib.sha3_512(sys.argv[1].encode()).hexdigest()
 binary = str(bin(int(result, 16)))[2:].rjust(512, '0')
+print(binary)
 difficulty = len(binary.split('1', 1)[0])
 print(difficulty)
